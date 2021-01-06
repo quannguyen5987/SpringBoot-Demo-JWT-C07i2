7b3448f23589a29d912fbcc155021028a4c9b210
quan200699 <51079189+quan200699@users.noreply.github.com>
2021-01-06 09:31:42 +0700

    add role service

---
+++ src/main/java/com/codegym/demo/service/role/IRoleService.java
@@ -0,0 +1,7 @@
+package com.codegym.demo.service.role;
+
+import com.codegym.demo.model.Role;
+import com.codegym.demo.service.IGeneralService;
+
+public interface IRoleService extends IGeneralService<Role> {
+}
